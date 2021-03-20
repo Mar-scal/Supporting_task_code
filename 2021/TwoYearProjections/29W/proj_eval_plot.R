e61bd901ccd48d3cc23fcd86367d60cc6533df63
Freya Keyser <freyakeyser@gmail.com>
2021-03-20 07:36:59 -0300

    new breaks for 29

--- 2021/TwoYearProjections/29W/proj_eval_plot.R
+++ 2021/TwoYearProjections/29W/proj_eval_plot.R
@@ -114,9 +114,17 @@ proj_eval_plot <- function(object, area, surplus, mu, ref.pts, save){
   
   if(area %in% c("29A", "29B", "29C", "29D")) {
     break1 <- seq(0,750, 250)
-    break2 <- seq(0,50, 10)
+    break2 <- seq(0,30, 5)
     break3 <- seq(-200,200, 50)
-    break4 <- seq(-2.5, 2.5, 1)
+    break4 <- seq(-3, 3, 1)
+  }
+  
+  if(area =="29B"){
+    break2 <- seq(0,20,5)
+    break4 <- seq(-3.5, 3.5, 1.75)
+  }
+  if(area =="29A"){
+    break2 <- seq(0,10,2)
   }
   
   # boxplot code here
